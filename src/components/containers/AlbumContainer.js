30b21e2aa53cd63df36e79237eefd6e0759e66da
Kamran Ali <kgali21@gmail.com>
2019-11-07 16:38:37 -0800

    got albums to display and page!

--- src/components/containers/AlbumContainer.js
+++ src/components/containers/AlbumContainer.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Albums from '../albums/Albums';
 import { getAlbums } from '../../services/api-call';
@@ -17,9 +17,17 @@ function FindAlbum({ match }) {
     }
   }, [page]);
 
+  const decrementPage = () => {
+    setPage(page - 1);
+  };
+
+  const incrementPage = () => {
+    setPage(page + 1);
+  };
+
   return (
     <div>
-      <Albums albums={albums} id={albums.id} name={match.params.name} incrementPage={page} decrementPage={page}  />
+      <Albums albums={albums} id={albums.id} name={match.params.name} incrementPage={incrementPage} decrementPage={decrementPage} />
     </div>
   );
 }
@@ -34,57 +42,3 @@ FindAlbum.propTypes = {
 };
 
 export default FindAlbum;
-
-
-// export default class AlbumContainer extends Component {
-
-// static propTypes = {
-//   match: PropTypes.shape({
-//     params: PropTypes.shape({
-//       id: PropTypes.string.isRequired,
-//       name: PropTypes.string.isRequired
-//     }).isRequired
-//   }).isRequired
-// }
-
-//   state = {
-//     albums: [],
-//     page: 0
-//   }
-
-// componentDidMount() {
-//   getAlbums(this.props.match.params.id, this.state.page)
-//     .then(albums => {
-//       this.setState({ albums });
-//     });
-// }
-
-//   decrementPage = () => {
-//     this.setState(state => ({
-//       page: state.page - 1
-//     }), () =>
-//       getAlbums(this.props.match.params.id, this.state.page)
-//         .then(albums => {
-//           this.setState({ albums });
-//         })
-//     );
-//   }
-
-//   incrementPage = () => {
-//     this.setState(state => ({
-//       page: state.page + 1
-//     }), () =>
-//       getAlbums(this.props.match.params.id, this.state.page)
-//         .then(albums => {
-//           this.setState({ albums });
-//         })
-//     );
-//   }
-
-//   render() {
-
-//     const { albums } = this.state;
-
-
-
-// }
